bcb29d7ee67a9f2fa27105516a554cb345bd399c
landycodes <landryandrewsk8@gmail.com>
2022-10-09 22:50:16 -0700

    add team manager works

--- index.js
+++ index.js
@@ -2,47 +2,60 @@
 //prompt questions and generate template html file
 const inquirer = require('inquirer');
 const fs = require('fs');
-const renderHtml = require('./src/template');
-
-const questions = [
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+//const renderHtml = require('./src/template');
+console.log('i pooped my pants')
+const managerQ = [
     {
         type: 'input',
-        name: 'Team-manager',
+        name: 'TeamManager',
         message: 'Who is the Team Manager?'
     },
     {
         type: 'input',
-        name: 'manager-id',
+        name: 'managerId',
         message: 'What is the Team Managers ID number?'
     },
     {
         type: 'input',
-        name: '',
-        message: ''
+        name: 'managerEmail',
+        message: 'What is the Team Managers E-mail?'
     },
     {
         type: 'input',
-        name: '',
-        message: ''
-    },
-    {
-        type: 'input',
-        name: '',
-        message: ''
-    },
+        name: 'officeNumber',
+        message: 'Enter Team Managers office number'
+    }
 ]
 
+const menu = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'Who would you like to bring aboard?',
+        choices: ['Engineer', 'Intern', 'I\'m done, make my webpage! ']
+}
+]
 function writeFile(answers) {
     fs.writeFile('index.html', answers, (err) => {
         err ? console.error(err) : console.log('***Your roster has been generated!***')
     })
 }
 
-inquirer.prompt(questions)
+inquirer.prompt(managerQ)
+//const menuPrompt = await inquirer.prompt(menu)
 .then((answers) => {
-    writeFile(renderHtml())
-    //console.log(`hello my name is ${answers.testQ}`)
-}) 
+    console.log(answers)
+    //console.log(managerQuestions)
+    //console.log(menuPrompt)
+    
+    const renderHtml = require('./src/template');
+    writeFile(renderHtml(answers));
+})
 
 //answers from questions go into there respective class
 //ex team manager = new Manager(answers from question in here)
--- src/template.js
+++ src/template.js
@@ -11,10 +11,37 @@ const Intern = require('../lib/Intern')
 // const guy4 = new Intern('bob', '69', 'boob@gmail.com', 'cienega')
 
 // console.log(guy1, guy2, guy3, guy4)
-
+//const manager = new Manager(data.TeamManager, data.managerId, data.managerEmail, data.officeNumber)
 // console.log(guy3.GitHub)
+//console.log('hello')
+//  let data = {
+//     TeamManager: 'andrew',
+//     managerId: '69',
+//     managerEmail: 'fake@email.com',
+//     officeNumber: '12'
+//   }
+function managerCard(data) { 
+    let manager = new Manager(data.TeamManager, data.managerId, data.managerEmail, data.officeNumber)
+    return `<div class="card bg-light w-25 m-3">
+        <div class="card-header bg-primary text-center">
+            <h4>${manager.Name}</h4>
+            <h6 class="card-subtitle mb-2 text-white">${manager.Role}</h6>
+        </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: #${manager.ID}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.Email}">${manager.Email}</a></li>
+                <li class="list-group-item">Office Number: ${manager.OfficeNum}</li>
+              </ul>            
+        </div>
+    </div>`;
+}
 
-function renderHtml() {
+function teamCards() {
+
+}
+
+function renderHtml(data) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -31,22 +58,13 @@ function renderHtml() {
     <body>
         <header class="jumbotron-fluid bg-primary text-center display-3 mb-2">Team</header> 
         <div class="container d-flex flex-wrap justify-content-center">
-            <!--card-->       
-            <div class="card bg-light w-25 m-3">
-                <div class="card-header bg-primary text-center">
-                    <h4>Name</h4>
-                    <h6 class="card-subtitle mb-2 text-white">Position</h6>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID: #</li>
-                        <li class="list-group-item">Email: <a href="mailto:EMAIL HERE">EMAIL HERE</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://github.com/GITHUB HERE"></a>GITHUB HERE</li>
-                      </ul>            
-                </div>
-            </div>
+            <!--Manager card*vvv*-->       
+            ${managerCard(data)}
+            <!--Engineer/intern cards*vvv*-->
+
         </div>
     </body>
     </html>`
 }
+
 module.exports = renderHtml;
